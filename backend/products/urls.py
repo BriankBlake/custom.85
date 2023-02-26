db5ee99b375df29aa26e6f15abf7a0f03e0211bd
brian blake <104259527+babyslikk@users.noreply.github.com>
2023-02-26 13:06:32 -0600

    created links for the product page

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path('', views.product, name='product'),
+
+    path('<slug:slug>/', views.product_info, name='product-info'),
 ]
 
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Product
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 def product(request):
 
-    return render(request, 'product/product.html')
+    all_products = Product.objects.all()
+
+    context = {'my_products': all_products}
+
+    return render(request, 'product/product.html', context)
 
 
 def categories(request):
@@ -14,3 +19,11 @@ def categories(request):
 
     return {'all_categories': all_categories}
 
+
+def product_info(request, slug):
+
+    product = get_object_or_404(Product, slug=slug)
+
+    context = {'product': product}
+
+    return render(request, 'product/product-info.html', context)
