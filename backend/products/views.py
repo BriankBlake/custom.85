f8ad29c6d9c6e5609e09a6aade11272f627c22c6
brian blake <104259527+babyslikk@users.noreply.github.com>
2023-02-26 10:39:26 -0600

    Created styling and css

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 def product(request):
 
     return render(request, 'product/product.html')
+
+
+def categories(request):
+
+    all_categories = Category.objects.all()
+
+    return {'all_categories': all_categories}
+
