30133c3cc98d93c9f0855cbdb3ded7067f2df459
brian blake <104259527+babyslikk@users.noreply.github.com>
2023-02-26 21:49:52 -0600

    updated the cart views page

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .cart import Cart
 from products.models import Product
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -17,12 +18,18 @@ def cart_add(request):
 
     cart = Cart(request)
 
-    if request.POST.get('action') == 'POST':
+    if request.POST.get('action') == 'post':
 
         product_id = int(request.POST.get('product_id'))
         product_quantity = int(request.POST.get('product_quantity'))
-        prouduct = get_object_or_404(Product, id=product_id)
-        cart.add(prouduct=prouduct, product_qty=product_quantity)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product, product_qty=product_quantity)
+
+        response = JsonResponse({'The product is called: ' : product.title, ' and the product quantity is: ': product_quantity})
+
+        return response
         
 
 def cart_delete(request):
